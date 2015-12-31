48fdc0151ceef6d2dfff27217e312da76f885a43
Mararok <mararok@gmail.com>
2015-12-31 14:32:45 +0100

    Added to repo

---
+++ src/main/com/mararok/epicwar/player/WarPlayerManager.java
@@ -0,0 +1,28 @@
+/**
+ * EpicWar
+ * The MIT License
+ * Copyright (C) 2015 Mararok <mararok@gmail.com>
+ */
+package com.mararok.epicwar.player;
+
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+import com.mararok.epicwar.War;
+import com.mararok.epicwar.faction.Faction;
+
+public interface WarPlayerManager {
+
+  public WarPlayer findByPlayer(Player nativePlayer) throws Exception;
+
+  public WarPlayer findByUUID(UUID uuid) throws Exception;
+
+  public WarPlayer register(Player nativePlayer, Faction faction) throws Exception;
+
+  public void update(WarPlayer player) throws Exception;
+
+  public void delete(WarPlayer player) throws Exception;
+
+  public War getWar();
+}
