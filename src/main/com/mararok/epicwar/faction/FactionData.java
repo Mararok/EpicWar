1d21033d51e059ad6a70e00dad39592d97ef98ed
Mararok <mararok@gmail.com>
2016-01-06 12:26:07 +0100

    Refactored

--- src/main/com/mararok/epicwar/faction/FactionData.java
+++ src/main/com/mararok/epicwar/faction/FactionData.java
@@ -5,10 +5,8 @@
  */
 package com.mararok.epicwar.faction;
 
-import java.util.List;
-
-import com.mararok.epiccore.Position3D;
 import com.mararok.epiccore.entity.EntityData;
+import com.mararok.epiccore.math.Position3D;
 
 public class FactionData extends EntityData {
   public String name;
@@ -19,19 +17,4 @@ public class FactionData extends EntityData {
   public Faction.BannerPattern bannerPattern;
 
   public Position3D spawnPosition;
-
-  @Override
-  public List<String> toStringList() {
-    List<String> list = super.toStringList();
-    list.add(name);
-    list.add(shortcut);
-    list.add(description);
-    list.add(Character.toString(color.getChatColor().getChar()));
-    list.add(bannerPattern.serialize());
-    list.add(Long.toString(spawnPosition.x));
-    list.add(Long.toString(spawnPosition.y));
-    list.add(Long.toString(spawnPosition.z));
-
-    return list;
-  }
 }
