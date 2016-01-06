69fcf02d1e728ee7f79120f571b7871fccf9a320
Mararok <mararok@gmail.com>
2016-01-06 12:17:39 +0100

    Refactored

--- src/main/com/mararok/epicwar/command/control/subsector/SubsectorInfoCommand.java
+++ src/main/com/mararok/epicwar/command/control/subsector/SubsectorInfoCommand.java
@@ -7,9 +7,9 @@ package com.mararok.epicwar.command.control.subsector;
 
 import org.bukkit.entity.Player;
 
-import com.mararok.epiccore.MessageBuilder;
 import com.mararok.epiccore.command.CommandArguments;
 import com.mararok.epiccore.command.CommandMetadata;
+import com.mararok.epiccore.misc.MessageBuilder;
 import com.mararok.epicwar.EpicWarPlugin;
 import com.mararok.epicwar.War;
 import com.mararok.epicwar.command.EpicWarCommand;
