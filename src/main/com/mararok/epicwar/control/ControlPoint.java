49f8145b4f3078b80b73a0accd78448d7bd90560
Mararok <mararok@gmail.com>
2016-01-16 10:37:15 +0100

    Replaced returns Power component with power manipulation  methods
    Added connections accessors

--- src/main/com/mararok/epicwar/control/ControlPoint.java
+++ src/main/com/mararok/epicwar/control/ControlPoint.java
@@ -5,8 +5,10 @@
  */
 package com.mararok.epicwar.control;
 
+import java.util.Collection;
+
 import com.mararok.epiccore.math.Position3D;
-import com.mararok.epicwar.control.internal.Occupation;
+import com.mararok.epicwar.faction.Faction;
 
 /**
  * This is a point that controls the set subsectors
@@ -22,20 +24,32 @@ public interface ControlPoint extends NamedControlArea {
   public int getRadius();
 
   /**
-   * @param radius In blocks unit and must be > 2
+   * @param radius In block unit and must be > 2
    */
   public void setRadius(int radius);
 
-  public ControlAreaPower getPower();
+  public int getCurrentPower();
 
-  public Occupation getOccupation();
+  public void setCurrentPower(int currentPower);
+
+  public int getMaxPower();
+
+  public void setMaxPower(int maxPower);
+
+  public boolean canCapture();
+
+  public boolean isFactionHasAnyConnection(Faction faction);
+
+  public boolean isConnectedTo(ControlPoint controlPoint);
+
+  public Collection<ControlPoint> getConnections();
+
+  public void connectTo(ControlPoint controlPoint);
+
+  public void disconnectFrom(ControlPoint controlPoint);
 
   public Sector getSector();
 
   public void setSector(Sector sector);
-
-  public ControlPoint[] getConnections();
-
-  public void connectTo(ControlPoint controlPoint);
 
 }
