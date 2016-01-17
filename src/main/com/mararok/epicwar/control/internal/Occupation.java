29f49b321a8f417d279c1ae8781412d55466ccd4
Mararok <mararok@gmail.com>
2016-01-17 13:57:35 +0100

    Refactored

--- src/main/com/mararok/epicwar/control/internal/Occupation.java
+++ src/main/com/mararok/epicwar/control/internal/Occupation.java
@@ -8,19 +8,20 @@ package com.mararok.epicwar.control.internal;
 import java.util.LinkedList;
 import java.util.List;
 
-import com.mararok.epicwar.control.ControlAreaPower;
+import com.mararok.epicwar.control.ControlPoint;
 import com.mararok.epicwar.control.event.ControlAreaEvent;
 import com.mararok.epicwar.control.event.PlayerAttackingControlAreaEvent;
 import com.mararok.epicwar.control.event.PlayerDefendingControlAreaEvent;
 import com.mararok.epicwar.control.event.PlayerLeavingControlAreaEvent;
 import com.mararok.epicwar.control.point.internal.ControlPointImpl;
+import com.mararok.epicwar.control.point.internal.ControlPointManagerImpl;
 import com.mararok.epicwar.faction.Faction;
 import com.mararok.epicwar.faction.FactionManager;
+import com.mararok.epicwar.faction.internal.FactionManagerImpl;
 import com.mararok.epicwar.player.WarPlayer;
 
 public class Occupation {
   private int defenders;
-  private int attackers;
   private List<WarPlayer> players = new LinkedList<WarPlayer>();
 
   private ControlPointImpl controlPoint;
@@ -30,83 +31,87 @@ public class Occupation {
   }
 
   public void addPlayer(WarPlayer player) {
-    if (isDefender(player)) {
-      addDefender(player);
-    } else {
-      addAttacker(player);
+    if (!players.contains(player)) {
+      if (isDefender(player)) {
+        ++defenders;
+        dispatchEvent(new PlayerDefendingControlAreaEvent(player, controlPoint));
+      } else {
+        ControlPointManagerImpl internal = (ControlPointManagerImpl) controlPoint.getWar().getControlPointManager();
+        internal.addOccupied(controlPoint);
+        dispatchEvent(new PlayerAttackingControlAreaEvent(player, controlPoint));
+      }
+
+      players.add(player);
     }
-
-    players.add(player);
-  }
-
-  private void addDefender(WarPlayer player) {
-    ++defenders;
-    dispatchEvent(new PlayerDefendingControlAreaEvent(player, controlPoint));
-  }
-
-  private void addAttacker(WarPlayer occupant) {
-    ++attackers;
-    dispatchEvent(new PlayerAttackingControlAreaEvent(occupant, controlPoint));
   }
 
   public void removePlayer(WarPlayer player) {
-    if (isDefender(player)) {
-      removeDefender(player);
-    } else {
-      removeAttacker(player);
+    if (players.contains(player)) {
+      if (isDefender(player)) {
+        --defenders;
+      } else {
+        ControlPointManagerImpl internal = (ControlPointManagerImpl) controlPoint.getWar().getControlPointManager();
+        internal.removeOccupied(controlPoint);
+      }
+
+      players.remove(player);
+      dispatchEvent(new PlayerLeavingControlAreaEvent(player, controlPoint));
     }
-
-    players.remove(player);
-    dispatchEvent(new PlayerLeavingControlAreaEvent(player, controlPoint));
-  }
-
-  private void removeDefender(WarPlayer player) {
-    --defenders;
-  }
-
-  private void removeAttacker(WarPlayer player) {
-    --defenders;
   }
 
   public boolean isDefender(WarPlayer player) {
     return player.getFaction() == controlPoint.getOwner();
   }
 
-  public void update() {
-    int defendersPower = getDefendersPower();
-    int attackersPower = getAttackersPower();
+  public void update() throws Exception {
+    int currentPower = controlPoint.getCurrentPower();
+    int defendersPower = calculateDefendersPower();
+    int attackersPower = calculateAttackersPower();
 
-    ControlAreaPower power = controlPoint.getPower();
-    power.set(power.getCurrent() + (defendersPower - attackersPower));
-    if (power.canCapture()) {
+    controlPoint.setCurrentPower(currentPower + (defendersPower - attackersPower));
+    if (controlPoint.canCapture()) {
       capture();
     }
 
   }
 
-  public int getDefendersPower() {
+  public int calculateDefendersPower() {
     return defenders * controlPoint.getWar().getSettings().controlPoint.powerDefender;
   }
 
-  public int getAttackersPower() {
+  public int calculateAttackersPower() {
+    boolean[] factionHasAnyConnection = new boolean[FactionManagerImpl.MAX_FACTIONS];
+    for (ControlPoint connection : controlPoint.getConnections()) {
+      if (connection.getOwner() != null) {
+        factionHasAnyConnection[connection.getOwner().getId()] = true;
+      }
+    }
+
+    int attackers = 0;
+    for (WarPlayer player : players) {
+      if (!isDefender(player) && factionHasAnyConnection[player.getFaction().getId()]) {
+        ++attackers;
+      }
+    }
+
     return attackers * controlPoint.getWar().getSettings().controlPoint.powerAttacker;
   }
 
-  private void capture() {
+  private void capture() throws Exception {
     Faction newOwner = findNewOwner();
     controlPoint.setOwner(newOwner);
-    controlPoint.getWar();
+    controlPoint.getWar().getControlPointManager().update(controlPoint);
   }
 
   private Faction findNewOwner() {
     FactionManager factionManager = controlPoint.getWar().getFactionManager();
-    short[] factionOccupants = new short[factionManager.findAll().size() + 1];
+    int[] factionOccupants = new int[FactionManagerImpl.MAX_FACTIONS];
 
     for (WarPlayer occupant : players) {
       ++factionOccupants[occupant.getFaction().getId()];
     }
 
-    short newDefenders = factionOccupants[1];
+    int newDefenders = factionOccupants[1];
     int newOwnerId = 1;
     for (int i = 2; i < factionOccupants.length; ++i) {
       if (newDefenders < factionOccupants[i]) {
@@ -115,44 +120,9 @@ public class Occupation {
       }
     }
 
-    attackers = defenders - (attackers - newDefenders);
     defenders = newDefenders;
-
     return factionManager.findById(newOwnerId);
   }
-
-  /*
-   * private void capture() {
-   * Faction faction = findNewOwner();
-   * 
-   * enemyOccupiersAmount -= newOccupant.amount;
-   * enemyOccupiersAmount += ownerOccupiersAmount;
-   * 
-   * info.ownerFactionID = newOccupant.factionID;
-   * ownerOccupiersAmount = newOccupant.amount;
-   * 
-   * info.power = 1;
-   * try {
-   * PreparedStatement st = DatabaseConnection.get().prepareQuery("UPDATE ew_ControlPoints SET ownerID = ? WHERE id = ?");
-   * st.setInt(1, info.ownerFactionID);
-   * st.setInt(2, getID());
-   * st.executeUpdate();
-   * DatabaseConnection.get().commit();
-   * } catch (SQLException e) {
-   * e.printStackTrace();
-   * }
-   * String newOwnerName = controlPoints.getWar().getFactionManager().getByID(info.ownerFactionID).getName();
-   * for (WarPlayerImpl player : occupiers) {
-   * if (player.getInfo().factionID == info.ownerFactionID) {
-   * player.sendMessage("Your faction captured " + getName() + " point");
-   * } else {
-   * player.sendMessage("Faction" + newOwnerName + " captured " + getName() + " point");
-   * }
-   * }
-   * controlPoints.onCapture(this);
-   * }
-   * 
-   */
 
   private void dispatchEvent(ControlAreaEvent event) {
     controlPoint.getWar().getPlugin().getEventManager().dispatchEvent(event);
