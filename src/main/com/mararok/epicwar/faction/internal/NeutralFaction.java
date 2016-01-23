5feb135349bbb558f2a32384325364b919aba935
Mararok <mararok@gmail.com>
2016-01-23 10:14:42 +0100

    Neutral Faction

---
+++ src/main/com/mararok/epicwar/faction/internal/NeutralFaction.java
@@ -0,0 +1,44 @@
+/**
+ * EpicWar
+ * The MIT License
+ * Copyright (C) 2016 Mararok <mararok@gmail.com>
+ */
+package com.mararok.epicwar.faction.internal;
+
+import org.bukkit.Location;
+
+import com.mararok.epicwar.War;
+import com.mararok.epicwar.faction.FactionData;
+
+public class NeutralFaction extends FactionImpl {
+
+  public NeutralFaction(FactionData data, War war) {
+    super(data, war);
+  }
+
+  @Override
+  public void setName(String newName) {
+  }
+
+  @Override
+  public void setShortcut(String newShortcut) {
+  }
+
+  @Override
+  public void setDescription(String newDescription) {
+  }
+
+  @Override
+  public void setBannerPattern(BannerPattern newBannerPattern) {
+  }
+
+  @Override
+  public void setSpawnLocation(Location location) {
+  }
+
+  @Override
+  public boolean isNeutral() {
+    return true;
+  }
+
+}
